be1a0819381d6ea2b778d58f2ea3e7e75ad57c57
Alexander Hardinan <blackchef.alex@gmail.com>
2025-11-24 00:33:22 +0700

    Remove deprecated UploadThingProvider file

--- src/providers/uploadthing-provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { UploadThingProvider } from "@uploadthing/react";
-
-export function UTProvider({ children }: { children: React.ReactNode }) {
-  return <UploadThingProvider>{children}</UploadThingProvider>;
-}
