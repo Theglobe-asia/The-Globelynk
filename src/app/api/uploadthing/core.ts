5f893b662892a989bb5884ecca6ca8d05b823adc
Alexander Hardinan <blackchef.alex@gmail.com>
2025-11-24 01:04:22 +0700

    Secure UploadThing router with CAlex password protection

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,15 +1,23 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { getServerSession } from "next-auth";
-import { authOptions } from "@/lib/auth";
 
 const f = createUploadthing();
 
+/**
+ * Password required to perform uploads
+ * You can later move this to an environment variable if you want.
+ */
+const ADMIN_PASSWORD = "CAlex";
+
 export const ourFileRouter = {
   coverUploader: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
-    .middleware(async () => {
-      const session = await getServerSession(authOptions);
-      if (!session?.user) throw new Error("Unauthorized");
-      return { userId: session.user.id };
+    .middleware(async ({ req }) => {
+      const pwd = req.headers.get("x-upload-password");
+
+      if (pwd !== ADMIN_PASSWORD) {
+        throw new Error("Unauthorized");
+      }
+
+      return { allowed: true };
     })
     .onUploadComplete(({ file }) => {
       return { url: file.url };
